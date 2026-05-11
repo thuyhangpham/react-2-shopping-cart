2a7b52226e16e81a4cd0ec70102416fbf8bc9cb2
thuyhangpham <phamthuyhang214@gmail.com>
2026-05-11 08:57:08 +0700

    config Redux Store

---
+++ shopping-cart/src/features/cartSlice.js
@@ -0,0 +1,42 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const savedCart = localStorage.getItem('cart');
+const initialState = {
+  items: savedCart ? JSON.parse(savedCart) : [],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const product = action.payload;
+      const existingItem = state.items.find(item => item.id === product.id);
+      
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...product, quantity: 1 });
+      }
+      localStorage.setItem('cart', JSON.stringify(state.items));
+    },
+    updateQuantity: (state, action) => {
+      const { id, amount } = action.payload; // amount có thể là 1 hoặc -1
+      const item = state.items.find(item => item.id === id);
+      if (item) {
+        item.quantity += amount;
+        if (item.quantity <= 0) {
+          state.items = state.items.filter(i => i.id !== id);
+        }
+      }
+      localStorage.setItem('cart', JSON.stringify(state.items));
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(item => item.id !== action.payload);
+      localStorage.setItem('cart', JSON.stringify(state.items));
+    }
+  }
+});
+
+export const { addToCart, updateQuantity, removeFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
